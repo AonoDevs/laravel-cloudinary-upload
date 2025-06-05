b5dd99085ab86da65914677bcc7b840b3bb3480d
Roriwars <robin2b2010@gmail.com>
2025-06-05 09:40:21 +0200

    update base extension

--- src/Helper.php
+++ src/Helper.php
@@ -9,32 +9,46 @@ use Illuminate\Support\Arr;
 class Helper{
     public static function fromBase64(string $base64File): UploadedFile
     {
-        // Get file data base64 string
+        if (!preg_match('/^data:(.*?);base64,/', $base64File, $matches)) {
+            throw new \InvalidArgumentException('Base64 string is not valid');
+        }
+
+        $mimeType = $matches[1];
+        $extension = match ($mimeType) {
+            'image/jpeg' => 'jpg',
+            'image/png' => 'png',
+            'image/webp' => 'webp',
+            'image/gif' => 'gif',
+            'application/pdf' => 'pdf',
+            default => null,
+        };
+
+        if (!$extension) {
+            throw new \RuntimeException("Unsupported MIME type: $mimeType");
+        }
+
         $fileData = base64_decode(Arr::last(explode(',', $base64File)));
 
-        // Create temp file and get its absolute path
         $tempFile = tmpfile();
         $tempFilePath = stream_get_meta_data($tempFile)['uri'];
 
-        // Save file data in file
         file_put_contents($tempFilePath, $fileData);
 
         $tempFileObject = new File($tempFilePath);
+        $filename = 'upload.' . $extension;
+
         $file = new UploadedFile(
             $tempFileObject->getPathname(),
-            $tempFileObject->getFilename(),
-            $tempFileObject->getMimeType(),
+            $filename,
+            $mimeType,
             0,
-            true // Mark it as test, since the file isn't from real HTTP POST.
+            true
         );
 
-        // Close this file after response is sent.
-        // Closing the file will cause to remove it from temp director!
         app()->terminating(function () use ($tempFile) {
             fclose($tempFile);
         });
 
-        // return UploadedFile object
         return $file;
     }
 }
